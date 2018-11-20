87d4279bb7f4f1944024749b9678c32020549606
roecodes <roe.j.emma@gmail.com>
2018-11-20 16:37:17 -0500

    successful API call

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,38 @@
 
-//this is where i tried to structure the request in the same way we did it in class yesteday, however it was giving a "same origin policy" error       
-// const newsApp = {}
-// newsApp.apiKey = "4135dcf939eb4bae959a26ff87fedc97"
-// newsApp.getArticle = function(){
-//     $.ajax({
-//         url: "https://api.nytimes.com/svc/topstories/v2/home.jsonp",
-//         method: "GET",
-//         data: {
-//             api-key: newsApp.apiKey
-//         }
-//     }).then(res => {
-//         console.log("success")
-//     })
-// }
+//this is where i tried to structure the request in the same way we did it in class yesteday, however it was giving a "same origin policy" error
+
+const newsApp = {};
+newsApp.apiKey = `4135dcf939eb4bae959a26ff87fedc97`;
+
+newsApp.getArticle = function(){
+        $.ajax({
+            url: `https://api.nytimes.com/svc/search/v2/articlesearch.json`,
+            method: 'GET',
+            dataType: 'json',
+            data: {
+                'api-key': newsApp.apiKey,
+                'begin_date': `${userDate}`,
+                'end_date': `${userDate}`,
+            },
+        }).then(res => {
+            console.log('hello');
+            console.log(res) 
+        })
+}
+
+const userDate = prompt("on what day were you born?");
+
+$(function () { // start document ready 
+    newsApp.init();
+}); // end of document ready 
 
 
-// this section i took directly from the nyt documentation here: https://developer.nytimes.com/top_stories_v2.json# 
-let url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-url += '?' + $.param({
-    'api-key': "4135dcf939eb4bae959a26ff87fedc97"
-});
-$.ajax({
-    url: url,
-    method: 'GET',
-}).then(function (result) {
-    console.log(result);
-});
+newsApp.init = function () {
+    newsApp.getArticle();
+}
 
 
 
 
-// $(function () {
-//     newsApp.init();
-// });
 
-// newsApp.init = function () {
-//     console.log("start");
-// }
+
